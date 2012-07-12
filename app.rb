80c0cf67996f03ee63d33754033d318731126c0f
Chris Breiding <chrisbreiding@gmail.com>
2012-07-12 16:37:47 -0400

    Initial commit

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'pony'
+require_relative '../secrets.rb'
+
+get '/' do
+  @title = 'CRB Web Development'
+  @asset_path = 'index'
+  @favicon = true
+  erb :index
+end
+
+post '/send-message' do
+  Pony.mail({
+    to: 'chris@chrisbreiding.com',
+    subject: "Website Contact Form Submission from #{params[:name]}",
+    html_body: erb(:email, layout: false),
+
+    via: :smtp,
+    via_options: {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+      user_name: 'chris@chrisbreiding.com',
+      password: GAPPS_PW,
+      authentication: :plain,
+      domain: 'crbdev.com'
+    }
+  })
+
+  '<div class="response valid">Your message has been sent - Thank you!</div>'
+end
