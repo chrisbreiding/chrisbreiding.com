c0237f98709d2823f8f2834c9d3404feba5497ce
Chris Breiding <chrisbreiding@gmail.com>
2012-10-13 16:53:46 -0400

    Add graceful degradation for contact form

--- app.rb
+++ app.rb
@@ -1,7 +1,32 @@
 require 'sinatra'
 require 'pony'
 require 'maruku'
+require 'rack-flash'
 require_relative '../secrets.rb'
+
+enable :sessions
+use Rack::Flash
+
+helpers do
+  def send_message
+    Pony.mail({
+      to: 'chris@chrisbreiding.com',
+      subject: "Website Contact Form Submission from #{params[:name]}",
+      html_body: erb(:email, layout: false),
+
+      via: :smtp,
+      via_options: {
+        address: 'smtp.gmail.com',
+        port: '587',
+        enable_starttls_auto: true,
+        user_name: 'admin@chrisbreiding.com',
+        password: GAPPS_PW,
+        authentication: :plain,
+        domain: 'chrisbreiding.com'
+      }
+    })
+  end
+end
 
 get '/' do
   @title = 'Chris Breiding | Web Developer'
@@ -9,22 +34,32 @@ get '/' do
 end
 
 post '/send-message' do
-  Pony.mail({
-    to: 'chris@chrisbreiding.com',
-    subject: "Website Contact Form Submission from #{params[:name]}",
-    html_body: erb(:email, layout: false),
 
-    via: :smtp,
-    via_options: {
-      address: 'smtp.gmail.com',
-      port: '587',
-      enable_starttls_auto: true,
-      user_name: 'admin@chrisbreiding.com',
-      password: GAPPS_PW,
-      authentication: :plain,
-      domain: 'chrisbreiding.com'
-    }
-  })
+  message_response = 'Your message has been sent - Thank you!'
 
-  '<div class="response valid">Your message has been sent - Thank you!</div>'
+  if request.xhr?
+    send_message
+    message_response
+  else
+    errors = {}
+    all_valid = true
+
+    [:name, :email, :message].each do |field|
+      if params[field].empty?
+        all_valid = false
+        errors[field] = true
+      end
+      flash[field] = params[field]
+    end
+
+    flash[:errors] = errors
+
+    if all_valid
+      send_message
+      flash[:message_response] = message_response
+    end
+
+    redirect to('/#contact'), 303
+  end
+
 end
