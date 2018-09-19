c36dd759999d2281617d89f49ea0689da9125cde
Chris Breiding <chrisbreiding@gmail.com>
2018-09-19 15:10:34 -0400

    &> /dev/null

--- src/scripts.js
+++ src/scripts.js
@@ -1,54 +1,15 @@
-(function () {
+$('.contact-form').on('submit', function (e) {
+  e.preventDefault();
 
-  var slice = Array.prototype.slice;
+  if ($('.processing').length) return;
 
-  var partial = function (fn) {
-    var args = slice.call(arguments, 1);
-    return function () {
-      fn.apply(this, args.concat(slice.call(arguments)));
-    };
-  };
+  $('.contact-form button')
+    .after($('<div class="processing">Sending...</div>'))
+    .remove();
 
-  var isValid = function ($input) {
-    return $input.val() !== '';
-  };
-
-  var checkValidity = function ($input, $form) {
-    var action = isValid($input) ? 'removeClass' : 'addClass';
-    $form[action]('invalid');
-  };
-
-  var closeForm = function ($form, success) {
+  setTimeout(function () {
     $form.find('.fields').slideUp(500, function () {
-      $form.addClass(success ? 'success' : 'failure');
+      $form.addClass('success');
     });
-  };
-
-  var sendMessage = function ($form) {
-    $.post($form.attr('action'), $form.serialize())
-    .then(function (response) {
-      closeForm($form, !response.reject_reason);
-    })
-    .catch(function () {
-      closeForm($form, false);
-    })
-  };
-
-  var onSubmit = function ($form, $message, e) {
-    e.preventDefault();
-
-    if ($('.processing').length || !isValid($message)) return;
-
-    $form
-      .find('button')
-      .after($('<div class="processing">Sending...</div>'))
-      .remove();
-    sendMessage($form);
-  };
-
-  var $contactForm = $('.contact-form');
-  var $message = $('.message');
-  $contactForm.on('submit', partial(onSubmit, $contactForm, $message));
-  $message.on('keyup', partial(checkValidity, $message, $contactForm));
-
-}());
+  }, 2000);
+});
